bb16bd033c3781948b99f9d7d728570b7b58f376
T4T4el7 <ochoaarmando13@gmail.com>
2019-10-23 16:54:59 -0700

    submition form wit himage upload made

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,1 +1,12 @@
-console.log("get bills");
+$("#infoSubmit").on("click", function() {
+    console.log("i work niggs");
+    let email = $("#customerEmail").val();
+    let name = $("#customerName").val();
+    let number = $("#customerNumber").val();
+    let customer = {
+        email: email,
+        name: name,
+        number: number
+    }
+    console.log(customer)
+});
